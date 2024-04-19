fffa74effe68b2bed9eb95d1445ac4639334da8a
Lucas Amorim Vasconcelos Torres <torres_lucas2@hotmail.com>
2024-04-19 16:26:21 -0300

    Banco com POO

--- main.py
+++ main.py
@@ -1,124 +1,129 @@
-def main():
-    LIMITE_SAQUES = 3
-    AGENCIA = "0001-9"
-    menu = """
-    [C] criar conta
-    [D] depositar
-    [R] retirar
-    [C] consultar
-    [U] atualizar
-    [D] deletar
-    [S] sair
-    """
-    contas = []
+class Conta:
+    def __init__(self, cpf, username, nome, saldo):
+        self.cpf = cpf
+        self.username = username
+        self.nome = nome
+        self.saldo = saldo
+        self.extrato = ""
+        self.numero_saques = 0
 
-    while True:
-        print(menu)
-        opcao = input("Escolha uma opção: ").upper()
-        if opcao == "C":
-            criar_conta(contas)
-        elif opcao == "D":
-            depositar(contas)
-        elif opcao == "R":
-            sacar(contas)
-        elif opcao == "C":
-            consultar(contas)
-        elif opcao == "U":
-            atualizar(contas)
-        elif opcao == "D":
-            deletar(contas)
-        elif opcao == "S":
-            break
-        else:
-            print("Opção inválida")
-        print()
-
-def criar_conta(contas):
-    cpf = input("Digite o CPF: ")
-    username = input("Digite o nome de usuário: ")
-    nome = input("Digite o nome: ")
-    saldo = float(input("Digite o saldo inicial: "))
-    extrato = ""
-    numero_saques = 0
-    conta = {
-        "cpf": cpf,
-        "username": username,
-        "nome": nome,
-        "saldo": saldo,
-        "extrato": extrato,
-        "numero_saques": numero_saques
-    }
-    contas.append(conta)
-    print("Conta criada com sucesso!")
-
-def depositar(contas):
-    cpf = input("Digite o CPF: ")
-    valor = float(input("Digite o valor a ser depositado: "))
-    conta = buscar_conta(contas, cpf)
-    if conta:
-        conta["saldo"] += valor
-        conta["extrato"] += f"Depósito: R$ {valor:.2f}\n"
+    def depositar(self, valor):
+        self.saldo += valor
+        self.extrato += f"Depósito: R$ {valor:.2f}\n"
         print("Depósito realizado com sucesso!")
-    else:
-        print("Conta não encontrada.")
 
-def sacar(contas):
-    cpf = input("Digite o CPF: ")
-    valor = float(input("Digite o valor a ser sacado: "))
-    conta = buscar_conta(contas, cpf)
-    if conta:
-        if conta["numero_saques"] < 3:
-            if conta["saldo"] >= valor:
-                conta["saldo"] -= valor
-                conta["extrato"] += f"Saque: R$ {valor:.2f}\n"
-                conta["numero_saques"] += 1
+    def sacar(self, valor):
+        if self.numero_saques < 3:
+            if self.saldo >= valor:
+                self.saldo -= valor
+                self.extrato += f"Saque: R$ {valor:.2f}\n"
+                self.numero_saques += 1
                 print("Saque realizado com sucesso!")
             else:
                 print("Saldo insuficiente")
         else:
             print("Limite de saques diários atingido")
-    else:
-        print("Conta não encontrada.")
 
-def consultar(contas):
-    cpf = input("Digite o CPF: ")
-    conta = buscar_conta(contas, cpf)
-    if conta:
-        print(f"CPF: {conta['cpf']}")
-        print(f"Nome de usuário: {conta['username']}")
-        print(f"Nome: {conta['nome']}")
-        print(f"Saldo: R$ {conta['saldo']:.2f}")
+    def consultar(self):
+        print(f"CPF: {self.cpf}")
+        print(f"Nome de usuário: {self.username}")
+        print(f"Nome: {self.nome}")
+        print(f"Saldo: R$ {self.saldo:.2f}")
         print("Extrato:")
-        print(conta["extrato"])
-    else:
-        print("Conta não encontrada.")
+        print(self.extrato)
 
-def atualizar(contas):
-    cpf = input("Digite o CPF: ")
-    conta = buscar_conta(contas, cpf)
-    if conta:
-        username = input("Digite o novo nome de usuário: ")
-        nome = input("Digite o novo nome: ")
-        conta["username"] = username
-        conta["nome"] = nome
-        print("Conta atualizada com sucesso!")
-    else:
-        print("Conta não encontrada.")
 
-def deletar(contas):
-    cpf = input("Digite o CPF: ")
-    conta = buscar_conta(contas, cpf)
-    if conta:
-        contas.remove(conta)
-        print("Conta deletada com sucesso!")
-    else:
-        print("Conta não encontrada.")
+class Banco:
+    def __init__(self):
+        self.contas = []
 
-def buscar_conta(contas, cpf):
-    for conta in contas:
-        if conta["cpf"] == cpf:
-            return conta
-    return None
+    def criar_conta(self, cpf, username, nome, saldo):
+        conta = Conta(cpf, username, nome, saldo)
+        self.contas.append(conta)
+        print("Conta criada com sucesso!")
+
+    def buscar_conta(self, cpf):
+        for conta in self.contas:
+            if conta.cpf == cpf:
+                return conta
+        return None
+
+    def deletar_conta(self, cpf):
+        conta = self.buscar_conta(cpf)
+        if conta:
+            self.contas.remove(conta)
+            print("Conta deletada com sucesso!")
+        else:
+            print("Conta não encontrada.")
+
+    def main(self):
+        menu = """
+        [C] criar conta
+        [D] depositar
+        [R] retirar
+        [C] consultar
+        [U] atualizar
+        [D] deletar
+        [S] sair
+        """
+
+        while True:
+            print(menu)
+            opcao = input("Escolha uma opção: ").upper()
+            if opcao == "C":
+                cpf = input("Digite o CPF: ")
+                username = input("Digite o nome de usuário: ")
+                nome = input("Digite o nome: ")
+                saldo = float(input("Digite o saldo inicial: "))
+                self.criar_conta(cpf, username, nome, saldo)
+            elif opcao == "D":
+                cpf = input("Digite o CPF: ")
+                valor = float(input("Digite o valor a ser depositado: "))
+                conta = self.buscar_conta(cpf)
+                if conta:
+                    conta.depositar(valor)
+                else:
+                    print("Conta não encontrada.")
+            elif opcao == "R":
+                cpf = input("Digite o CPF: ")
+                valor = float(input("Digite o valor a ser sacado: "))
+                conta = self.buscar_conta(cpf)
+                if conta:
+                    if conta.saldo >= valor:
+                        conta.sacar(valor)
+                    else:
+                        print("Saldo insuficiente.")
+                else:
+                    print("Conta não encontrada.")
+            elif opcao == "C":
+                cpf = input("Digite o CPF: ")
+                conta = self.buscar_conta(cpf)
+                if conta:
+                    conta.consultar()
+                else:
+                    print("Conta não encontrada.")
+            elif opcao == "U":
+                cpf = input("Digite o CPF: ")
+                conta = self.buscar_conta(cpf)
+                if conta:
+                    username = input("Digite o novo nome de usuário: ")
+                    nome = input("Digite o novo nome: ")
+                    conta.username = username
+                    conta.nome = nome
+                    print("Conta atualizada com sucesso!")
+                else:
+                    print("Conta não encontrada.")
+            elif opcao == "D":
+                cpf = input("Digite o CPF: ")
+                self.deletar_conta(cpf)
+            elif opcao == "S":
+                break
+            else:
+                print("Opção inválida")
+            print()
+
 
 if __name__ == "__main__":
-    main()
+    banco = Banco()
+    banco.main()
+
